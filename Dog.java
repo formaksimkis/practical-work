281c557e4a8c2f9a8c1ffbd9bf1c889f4a0abad8
formaksimkis <58445270+formaksimkis@users.noreply.github.com>
2019-12-04 22:46:52 +0300

    new field "owner" in Dog

--- Dog.java
+++ Dog.java
@@ -3,16 +3,19 @@ public class Dog {
     String name;
     int age;
 	int weight;
+    String owner;
 
     public static void main(String[] args) {
         Dog bimka = new Dog();
         bimka.age = 3;
         bimka.name = "Bimka";
 		bimka.weight = 23;
+	    bimka.owner = "Maks";
         Dog laika = new Dog();
         laika.age = 3;
         laika.name = "Laika";
 		laika.weight = 23;
+	    laika.owner = "Sveta";
     }
 
 }
