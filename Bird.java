d2583f3c8f22f279649730858973658238f8835a
Maksim Kiselev <formaksimkis@gmail.com>
2019-12-04 21:41:44 +0300

    update class Bird (new instance Petrusha)

--- Bird.java
+++ Bird.java
@@ -1,7 +1,14 @@
-public class Dog {
+public class Bird {
 
     String name;
     int age;
 	String colour;
+	
+	public static void main(String[] args) {
+        Bird petrusha = new Bird();
+        petrusha.age = 2;
+        petrusha.name = "Petrusha";
+		petrusha.colour = "Yellow";
+    }
 
 }
