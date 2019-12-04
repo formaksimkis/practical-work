defccbc45299930d61f32936ce3734d0639616bb
Maksim Kiselev <formaksimkis@gmail.com>
2019-12-04 22:49:42 +0300

    new field "speed" in Dog

--- Dog.java
+++ Dog.java
@@ -3,16 +3,19 @@ public class Dog {
     String name;
     int age;
 	int weight;
+	double speed;
 
     public static void main(String[] args) {
         Dog bimka = new Dog();
         bimka.age = 3;
         bimka.name = "Bimka";
 		bimka.weight = 23;
+		bimka.speed = 30;
         Dog laika = new Dog();
         laika.age = 3;
         laika.name = "Laika";
 		laika.weight = 23;
+		laika.speed = 33;
     }
 
 }
