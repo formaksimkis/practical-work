6ad80265932d38eb9d4d317309b8a88cea87ad8e
Maksim Kiselev <formaksimkis@gmail.com>
2019-12-04 21:20:06 +0300

    update classes Cat and Dog

--- Cat.java
+++ Cat.java
@@ -2,5 +2,13 @@ public class Cat {
 
     String name;
     int age;
+	String sex;
+
+    public static void main(String[] args) {
+        Cat tima = new Cat();
+        tima.age = 5;
+        tima.name = "Tima";
+		tima.sex = "male";
+    }
 
 }
--- Dog.java
+++ Dog.java
@@ -4,4 +4,11 @@ public class Dog {
     int age;
 	int weight;
 
+    public static void main(String[] args) {
+        Dog laika = new Dog();
+        laika.age = 3;
+        laika.name = "Laika";
+		laika.weight = 23;
+    }
+
 }
